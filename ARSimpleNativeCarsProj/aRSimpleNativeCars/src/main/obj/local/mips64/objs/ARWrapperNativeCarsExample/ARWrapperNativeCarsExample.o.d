3cc07f049b6043b387a06b80527f6bac3e09b53f
miadaradici <mia.daradici@gmail.com>
2016-04-23 19:49:48 +0300

    proiect

---
+++ ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/obj/local/mips64/objs/ARWrapperNativeCarsExample/ARWrapperNativeCarsExample.o.d
@@ -0,0 +1,245 @@
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/obj/local/mips64/objs/ARWrapperNativeCarsExample/ARWrapperNativeCarsExample.o: \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/ARWrapperNativeCarsExample.cpp \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/gsub_es.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/gl.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/glplatform.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\KHR/khrplatform.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/cdefs.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/cdefs_elf.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\android/api-level.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdint.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\machine/wchar_limits.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/glext.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/config.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/ar.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdio.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdio.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/int-l64.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/bitsperlong.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/bitsperlong.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/posix_types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/stddef.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/compiler.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/posix_types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/sgidefs.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/posix_types.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/sysmacros.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdlib.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\string.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\malloc.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\xlocale.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\alloca.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\strings.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\memory.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/errno.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\errno.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\errno.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/errno.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/errno.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/errno-base.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/arConfig.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\jni.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\android/log.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/matrix.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\math.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\math.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\limits.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/limits.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/limits.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/syslimits.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/icp.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/icpCore.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/param.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/arImageProc.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\glStateCache.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/glm.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/Eden.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/EdenSurfaces.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/ARToolKitWrapperExportedAPI.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Platform.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Image.h \
+  /cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Error.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\unistd.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/select.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/time.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/time.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\signal.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/sigcontext.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/signal.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/signal.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/signal-defs.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/siginfo.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/siginfo.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/ucontext.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/user.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/sysconf.h \
+  /cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\pathconf.h
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/gsub_es.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/gl.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/glplatform.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\KHR/khrplatform.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/cdefs.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/cdefs_elf.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\android/api-level.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdint.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\machine/wchar_limits.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\GLES/glext.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/config.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/ar.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdio.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdio.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/int-l64.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/bitsperlong.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/bitsperlong.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/posix_types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/stddef.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/compiler.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/posix_types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/sgidefs.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/posix_types.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/sysmacros.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\stdlib.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\string.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\malloc.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\xlocale.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\alloca.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\strings.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\memory.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/errno.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\errno.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\errno.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/errno.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/errno.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/errno-base.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/arConfig.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\jni.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\android/log.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/matrix.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/../../android/support/include\math.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\math.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\limits.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/limits.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/limits.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/syslimits.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/icp.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/icpCore.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/param.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\AR/arImageProc.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\glStateCache.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/glm.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/Eden.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\Eden/EdenSurfaces.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/ARToolKitWrapperExportedAPI.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Platform.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Image.h:
+
+/cygdrive/d/Licenta/artoolkit5-master/AndroidStudioProjects/ARSimpleNativeCarsProj/aRSimpleNativeCars/src/main/jni/../../../../../../android/../include\ARWrapper/Error.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\unistd.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/select.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/time.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/time.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\signal.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/sigcontext.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\linux/signal.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/signal.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/signal-defs.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm/siginfo.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\asm-generic/siginfo.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/ucontext.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/user.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\sys/sysconf.h:
+
+/cygdrive/d/Android/android-sdk/ndk-bundle/platforms/android-21/arch-mips64/usr/include\pathconf.h:
